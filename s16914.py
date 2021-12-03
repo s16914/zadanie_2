8e1d10f68e1ac3a1526cb4fb1466b1ecdedfbbf8
s16914 <80533249+s16914@users.noreply.github.com>
2021-12-03 19:10:17 +0100

    Add files via upload

---
+++ s16914.py
@@ -0,0 +1,111 @@
+from azure.identity import ClientSecretCredential
+from azure.identity._credentials.default import DefaultAzureCredential 
+from azure.mgmt.resource import ResourceManagementClient
+from azure.mgmt.datafactory import DataFactoryManagementClient
+from azure.mgmt.datafactory.models import *
+from datetime import datetime, timedelta
+import time
+
+def print_item(group):
+    """Print an Azure object instance."""
+    print("\tName: {}".format(group.name))
+    print("\tId: {}".format(group.id))
+    if hasattr(group, 'location'):
+        print("\tLocation: {}".format(group.location))
+    if hasattr(group, 'tags'):
+        print("\tTags: {}".format(group.tags))
+    if hasattr(group, 'properties'):
+        print_properties(group.properties)
+
+def print_properties(props):
+    """Print a ResourceGroup properties instance."""
+    if props and hasattr(props, 'provisioning_state') and props.provisioning_state:
+        print("\tProperties:")
+        print("\t\tProvisioning State: {}".format(props.provisioning_state))
+    print("\n\n")
+
+def print_activity_run_details(activity_run):
+    """Print activity run details."""
+    print("\n\tActivity run details\n")
+    print("\tActivity run status: {}".format(activity_run.status))
+    if activity_run.status == 'Succeeded':
+        print("\tNumber of bytes read: {}".format(activity_run.output['dataRead']))
+        print("\tNumber of bytes written: {}".format(activity_run.output['dataWritten']))
+        print("\tCopy duration: {}".format(activity_run.output['copyDuration']))
+    else:
+        print("\tErrors: {}".format(activity_run.error['message']))
+
+
+def main():
+
+
+    subscription_id = 'ab55b103-2e54-4066-a7f4-b2a4fdaf7482'
+
+
+    rg_name = 'rg-mk'
+
+    df_name = 'mkadfaa'
+
+    credentials = DefaultAzureCredential()
+    resource_client = ResourceManagementClient(credentials, subscription_id)
+    adf_client = DataFactoryManagementClient(credentials, subscription_id)
+
+    rg_params = {'location':'East US'}
+    df_params = {'location':'East US'}
+
+    df = adf_client.factories.get(rg_name, df_name)
+
+    ls_name = 'AzureBlobStorage1'
+
+    storage_string = SecureString(value='DefaultEndpointsProtocol=https;AccountName=strmkf;AccountKey=9lzkgKoFm7B1yMkqvskD0usLtaJKPLuj7mmHKunZAdLw3z06R/p1n4YuUMyrisUfbADj+Tw/ZpGyCNNDkk7B3Q==;EndpointSuffix=core.windows.net')
+
+    ls_azure_storage = LinkedServiceResource(properties=AzureStorageLinkedService(connection_string=storage_string)) 
+    ls = adf_client.linked_services.create_or_update(rg_name, df_name, ls_name, ls_azure_storage)
+    print_item(ls)
+
+    ds_name = 'inputData'
+    ds_ls = LinkedServiceReference(reference_name=ls_name)
+    blob_path = 'adfdata/inputData'
+    blob_filename = 'rekord.txt'
+    ds_azure_blob = DatasetResource(properties=AzureBlobDataset(
+        linked_service_name=ds_ls, folder_path=blob_path, file_name=blob_filename))
+    ds = adf_client.datasets.create_or_update(
+        rg_name, df_name, ds_name, ds_azure_blob)
+    print_item(ds)
+
+    dsOut_name = 'outputData'
+    output_blobpath = 'adfdata/outputData2'
+    dsOut_azure_blob = DatasetResource(properties=AzureBlobDataset(linked_service_name=ds_ls, folder_path=output_blobpath))
+    dsOut = adf_client.datasets.create_or_update(
+        rg_name, df_name, dsOut_name, dsOut_azure_blob)
+    print_item(dsOut)
+
+    act_name = 'copyBlobtoBlob'
+    blob_source = BlobSource()
+    blob_sink = BlobSink()
+    dsin_ref = DatasetReference(reference_name=ds_name)
+    dsOut_ref = DatasetReference(reference_name=dsOut_name)
+    copy_activity = CopyActivity(name=act_name, inputs=[dsin_ref], outputs=[
+                                 dsOut_ref], source=blob_source, sink=blob_sink)
+
+    p_name = 'copyPipeline'
+    params_for_pipeline = {}
+    p_obj = PipelineResource(
+        activities=[copy_activity], parameters=params_for_pipeline)
+    p = adf_client.pipelines.create_or_update(rg_name, df_name, p_name, p_obj)
+    print_item(p)
+
+    run_response = adf_client.pipelines.create_run(rg_name, df_name, p_name, parameters={})
+
+    time.sleep(30)
+    pipeline_run = adf_client.pipeline_runs.get(
+        rg_name, df_name, run_response.run_id)
+    print("\n\tPipeline run status: {}".format(pipeline_run.status))
+    filter_params = RunFilterParameters(
+        last_updated_after=datetime.now() - timedelta(1), last_updated_before=datetime.now() + timedelta(1))
+    query_response = adf_client.activity_runs.query_by_pipeline_run(
+        rg_name, df_name, pipeline_run.run_id, filter_params)
+    print_activity_run_details(query_response.value[0])
+
+
+main()
